37037d72489d8030f27d2561858f76302d7f7abe
Bryan Lee <bryanmylee@gmail.com>
2024-10-21 18:20:28 +0800

    chore: use inter font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.svelte'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['InterVariable', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
 	plugins: []
 };
