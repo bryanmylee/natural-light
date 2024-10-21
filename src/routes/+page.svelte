0c763f66ed68e6e2b903a70ae3381a896fcda1d9
Bryan Lee <bryanmylee@gmail.com>
2024-10-21 12:56:25 +0800

    chore: themed slider

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 	import * as rgb from '$lib/color/rgb';
 	import * as hsl from '$lib/color/hsl';
 	import { pipe } from '$lib/pipe';
+	import TemperatureSlider from './TemperatureSlider.svelte';
 
 	const temperatureKelvin = transformed(
 		useLocalStorage('temperature_in_kelvin', '6595'),
@@ -15,28 +16,37 @@
 		(output) => String(output)
 	);
 	$: temperatureHsl = pipe($temperatureKelvin, rgb.fromTemperature, hsl.fromRgb);
-	$: textHsl = pipe(temperatureHsl, hsl.darken(0.8), hsl.desaturate(0.5));
 </script>
 
 <div
-	class="fixed inset-0 flex flex-col items-center"
-	style="background-color: {hsl.toCssProperty(temperatureHsl)}"
+	class="fixed inset-0 flex flex-col items-center bg-[--temp-paper]"
+	style:--temp-paper={pipe(temperatureHsl, hsl.toCssProperty)}
+	style:--temp-ink={pipe(temperatureHsl, hsl.darken(0.9), hsl.desaturate(0.1), hsl.toCssProperty)}
+	style:--temp-track-empty={pipe(
+		temperatureHsl,
+		hsl.darken(0.6),
+		hsl.desaturate(0.6),
+		hsl.toCssProperty
+	)}
+	style:--temp-track-filled={pipe(
+		temperatureHsl,
+		hsl.darken(0.3),
+		hsl.desaturate(0.5),
+		hsl.toCssProperty
+	)}
+	style:--temp-highlight={pipe(
+		temperatureHsl,
+		hsl.darken(-0.05),
+		hsl.desaturate(0.2),
+		hsl.toCssProperty
+	)}
 >
 	<div class="flex-1 container mx-auto flex flex-col">
 		<div class="flex-1" />
 		<div class="p-8 flex flex-col gap-4">
-			<input
-				type="range"
-				name="temperature"
-				bind:value={$temperatureKelvin}
-				min={4000}
-				max={9000}
-			/>
-			<p
-				class="font-bold text-[clamp(0.9rem,1rem+4vw,10rem)]"
-				style="color: {hsl.toCssProperty(textHsl)}"
-			>
-				<span>{$temperatureKelvin}</span> K
+			<TemperatureSlider bind:value={$temperatureKelvin} />
+			<p class="font-bold text-[clamp(0.9rem,1rem+4vw,10rem)] text-[--temp-ink]">
+				<span>{Math.floor($temperatureKelvin)}</span> K
 			</p>
 		</div>
 	</div>
---
+++ src/routes/TemperatureSlider.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import type { Kelvin } from '$lib/color/temperature';
+	import { createSlider, melt } from '@melt-ui/svelte';
+	import { spring } from 'svelte/motion';
+
+	export let value: Kelvin;
+
+	const smoothValue = spring([value]);
+	$: value = $smoothValue[0];
+
+	const {
+		elements: { root, range, thumbs }
+	} = createSlider({
+		value: smoothValue,
+		min: 3000,
+		max: 9000,
+		step: 1
+	});
+</script>
+
+<span use:melt={$root} class="relative flex h-[20px] items-center">
+	<span class="h-1 w-full bg-[--temp-track-empty]">
+		<span {...$range} use:range class="h-1 bg-[--temp-track-filled]" />
+	</span>
+
+	<span
+		use:melt={$thumbs[0]}
+		class="h-5 w-5 rounded-full bg-[--temp-track-filled] focus-visible:outline-none focus-visible:ring-4 ring-[--temp-paper]"
+	/>
+</span>
